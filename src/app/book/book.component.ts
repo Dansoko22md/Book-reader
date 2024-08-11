e7ccdcc923c6564dd8f169ecfc302977bf2f39c5
Dansoko22md <101418500+Dansoko22md@users.noreply.github.com>
2024-08-11 02:04:08 +0100

    updated

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -12,10 +12,11 @@ export class BookComponent implements OnInit, AfterViewInit {
   private book: any;
   private rendition1: any;
   private rendition2: any;
-  private currentLocation: any;
+  private currentLocation1: any;
+  private currentLocation2: any;
   bookId?: number;
 
-  constructor(private route: ActivatedRoute) {} 
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -37,7 +38,7 @@ export class BookComponent implements OnInit, AfterViewInit {
 
   loadBook(): void {
     console.log('Loading book...');
-    this.book = ePub(`assets/books/book${this.bookId}.epub`); 
+    this.book = ePub(`assets/books/book${this.bookId}.epub`);
     this.rendition1 = this.book.renderTo('viewer-1', {
       width: '100%',
       height: '100%'
@@ -48,61 +49,95 @@ export class BookComponent implements OnInit, AfterViewInit {
       height: '100%'
     });
 
-    this.rendition1.display().then(() => {
-      this.syncViewers();
-      console.log('Book displayed in viewer-1');
-    }).catch((error: any) => {
-      console.error('Error displaying book in viewer-1:', error);
+    this.book.ready.then(() => {
+      this.book.locations.generate(1000).then(() => {
+        this.rendition1.display().then(() => {
+          this.currentLocation1 = this.rendition1.currentLocation()?.start.cfi;
+          this.displayNextPageInViewer2();
+          this.updatePageCounter();
+          console.log('Page displayed in viewer-1');
+        }).catch((error: any) => {
+          console.error('Error displaying page in viewer-1:', error);
+        });
+      });
     });
 
     this.rendition1.on('relocated', (location: any) => {
-      this.currentLocation = location;
+      this.currentLocation1 = location?.start?.cfi;
       this.updatePageCounter();
-      this.syncViewers();
     });
 
-    this.book.ready.then(() => {
-      this.book.locations.generate(1000).then(() => {
-        this.updatePageCounter();
-      });
+    this.rendition2.on('relocated', (location: any) => {
+      this.currentLocation2 = location?.start?.cfi;
     });
   }
 
-  syncViewers(): void {
-    if (this.currentLocation) {
-      const nextCfi = this.book.locations.cfiFromLocation(this.currentLocation.end.location);
-      this.rendition2.display(nextCfi).then(() => {
+  displayNextPageInViewer2(): void {
+    this.rendition2.display(this.currentLocation1).then(() => {
+      this.rendition2.next().then(() => {
+        this.currentLocation2 = this.rendition2.currentLocation()?.start.cfi;
         console.log('Next page displayed in viewer-2');
       }).catch((error: any) => {
         console.error('Error displaying next page in viewer-2:', error);
+      });
+    }).catch((error: any) => {
+      console.error('Error displaying current page in viewer-2:', error);
+    });
+  }
+
+  displayPreviousPageInViewer2(): void {
+    if (this.currentLocation1 !== this.book.locations[0].start.cfi) {
+      this.rendition2.prev().then(() => {
+        this.currentLocation2 = this.rendition2.currentLocation()?.start.cfi;
+        console.log('Previous page displayed in viewer-2');
+      }).catch((error: any) => {
+        console.error('Error displaying previous page in viewer-2:', error);
       });
     }
   }
 
   updatePageCounter(): void {
     const pageCounter = document.getElementById('page-counter');
-    if (pageCounter && this.currentLocation) {
-      const currentPage = this.book.locations.locationFromCfi(this.currentLocation.start.cfi) + 1;
-      const totalPages = this.book.locations.total + 1;
+    if (pageCounter && this.currentLocation1) {
+      const currentPage = this.book.locations.locationFromCfi(this.currentLocation1) + 1;
+      const totalPages = this.book.locations.total;
       pageCounter.innerText = `Page ${currentPage} of ${totalPages}`;
     }
   }
 
   previousPage(): void {
-    this.rendition1.prev().then(() => {
-      this.syncViewers();
-      console.log('Previous page displayed in viewer-1');
-    }).catch((error: any) => {
-      console.error('Error displaying previous page in viewer-1:', error);
-    });
+    if (this.book.locations.locationFromCfi(this.currentLocation1) > 0) {
+      this.rendition2.prev().then(() => {
+        this.currentLocation2 = this.rendition2.currentLocation()?.start.cfi;
+        this.rendition1.prev().then(() => {
+          this.currentLocation1 = this.rendition1.currentLocation()?.start.cfi;
+          this.updatePageCounter();
+          console.log('Previous pages displayed in both viewers');
+        }).catch((error: any) => {
+          console.error('Error displaying previous page in viewer-1:', error);
+        });
+      }).catch((error: any) => {
+        console.error('Error displaying previous page in viewer-2:', error);
+      });
+    } else {
+      console.log('Already at the first page, cannot go back further.');
+    }
   }
 
   nextPage(): void {
-    this.rendition1.next().then(() => {
-      this.syncViewers();
-      console.log('Next page displayed in viewer-1');
-    }).catch((error: any) => {
-      console.error('Error displaying next page in viewer-1:', error);
-    });
+    const totalPages = this.book.locations.total;
+
+    if (this.book.locations.locationFromCfi(this.currentLocation1) < totalPages - 1) {
+      this.rendition1.next().then(() => {
+        this.currentLocation1 = this.rendition1.currentLocation()?.start.cfi;
+        this.displayNextPageInViewer2();
+        this.updatePageCounter();
+        console.log('Next pages displayed in both viewers');
+      }).catch((error: any) => {
+        console.error('Error displaying next pages:', error);
+      });
+    } else {
+      console.log('Already at the last page, cannot go forward.');
+    }
   }
 }
